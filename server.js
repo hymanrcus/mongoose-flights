a5d16b0ce429c405821a4641e013db00bb6a5917
Marcus <hymanmarcus95@gmail.com>
2022-09-27 19:35:27 -0400

    add event Listener to mongoose connection

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
-
+import "dotenv/config.js"
+import('./config/database.js')
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as usersRouter } from './routes/users.js'
